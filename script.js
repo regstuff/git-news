8b2f3f5249a833cfdf5913aefa763e58a52bd6f6
regstuff <btech11+github@gmail.com>
2022-06-06 12:04:41 +0530

    Use div columns to stack cards vertically

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
+    populateFeedList()
+});
 
-
-
-$.each(rss2json,function(listIndex, listElement){
-    console.log('Looping through RSS Category:', listIndex)
-    //$('body').append(`<div class="cardContainer"><h1 class="rssCard" id="title_${listIndex}">${listIndex}</h1><div class="rssCard" id="card_${listIndex}"><ul class="rssList">content_${listIndex}</ul></div></div>`)
-    var newfeed = ''
-    $.each(listElement, function (index, value) {
-        console.log('Looping through RSS URL:', value['feed']['title'])
-        $.each(value['entries'], function(index, data) {  
-            console.log('Looping through title:', data['title'])
-            newfeed = newfeed + parseFeed(data, value['feed']['title'])
+// Populate body element with list of RSS feeds
+function populateFeedList() {
+    $.each(rss2json,function(listIndex, listElement){
+        console.log('Looping through RSS Category:', listIndex)
+        //$('body').append(`<div class="cardContainer"><h1 class="rssCard" id="title_${listIndex}">${listIndex}</h1><div class="rssCard" id="card_${listIndex}"><ul class="rssList">content_${listIndex}</ul></div></div>`)
+        var newfeed = ''
+        $.each(listElement, function (index, value) {
+            console.log('Looping through RSS URL:', value['feed']['title'])
+            $.each(value['entries'], function(index, data) {  
+                console.log('Looping through title:', data['title'])
+                newfeed = newfeed + parseFeed(data, value['feed']['title'])
+            });
         });
+        if (newfeed != '') { // Create card if feed is not empty
+            let columnNum = `rssColumn_${$('.cardContainer').length % 3}` // Calculate number of existing elements of class cardContainer. Decide which column to place in.
+            appendColumn(columnNum, listIndex, newfeed)
+        }
     });
-    if (newfeed != '') { // Create card if feed is not empty
-        $('body').append(`<div class="cardContainer"><h1 class="rssCard" id="title_${listIndex}">${listIndex.replaceAll('_','/')}</h1><div class="rssCard" id="card_${listIndex}"><ul class="rssList">content_${listIndex}</ul></div></div>`)
-        $(`#card_${listIndex} > ul.rssList`).html(newfeed);
-    }
-});
+}
 
+// Function that appends html content to a div of id rssColumn_x
+function appendColumn(columnNum, listIndex, newfeed) {
+    console.log('In append function')
+    $(`#${columnNum}`).append(`<div class="cardContainer"><h1 class="rssCard" id="title_${listIndex}">${listIndex.replaceAll('_','/')}</h1><div class="rssCard" id="card_${listIndex}"><ul class="rssList">content_${listIndex}</ul></div></div>`)
+    $(`#card_${listIndex} > ul.rssList`).html(newfeed);
+}
 
-
-
-
-});
-
-
-
+// Parse the JSON dump and return html
 function parseFeed(data, value) { // https://stackoverflow.com/a/7067582/3016570
     let newsPublisher = value
     let newfeed = ''
@@ -48,7 +51,7 @@ function itemValid(item) { // Checks to see if item should be included in feed
     timediff = new Date($.now()).getTime() - new Date(item['pubDate']).getTime(); // Get timediff between now and when this article was published
     termExcluded = config['excludeTerms'].some(function(v) { return item['title'].indexOf(v) >= 0; }) || config['excludeTerms'].some(function(v) { return item['title'].indexOf(v.toUpperCase()) >= 0; }) // True if title contains any of the terms (as is & capitalized) to exclude - https://stackoverflow.com/a/5582621/3016570
     termIncluded = config['includeTerms'].some(function(v) { return item['title'].indexOf(v) >= 0; }) // True if title contains any of the terms to include
-    //boolval = !(timediff > config['maxPublishTime']*60*1000 || (termExcluded && !termIncluded)) // Check all conditions
-    boolval = !((termExcluded && !termIncluded)) // Check all conditions
+    boolval = !(timediff > config['maxPublishTime']*60*1000 || (termExcluded && !termIncluded)) // Check all conditions
+    //boolval = !((termExcluded && !termIncluded)) // Check all conditions
     return boolval
 }
