30c430b25a7b8392a0f5f8603324df598939f8b9
regstuff <btech11+github@gmail.com>
2022-06-19 14:36:00 +0530

    INclude non-http(s) urls for gnews-ed rss feed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     'maxPublishTime': 1*24*60, // In minutes
     'maxDescLen': 200, // Max number of characters
-    'excludeTerms': ['NEET', 'monkeypox', 'covid', 'COVID', 'corona', 'Moosewala', 'Nupur Sharma', 'Moose Wala', 'iTunes', 'iPhone', 'iPad', 'smartwatch', 'iOS', 'Watch', 'macOS', 'watchOS', 'The Wire', 'Scroll.in'], // Exclude any titles with these terms. Case-sensitive
+    'excludeTerms': ['NEET', 'monkeypox', 'covid', 'COVID', 'corona', 'Moosewala', 'Nupur Sharma', 'Moose Wala', 'iTunes', 'iPhone', 'iPad', 'smartwatch', 'iOS', 'Watch', 'macOS', 'watchOS'], // Exclude any titles with these terms. Case-sensitive
     'includeTerms': [], // Include any titles with these terms. Overrides exclude terms. Case-sensitive
     'rssurl': {'Scitech': ['https://www.wired.com/feed/tag/ai/latest/rss', 'https://www.wired.com/feed/category/science/latest/rss', 'https://www.wired.com/feed/category/security/latest/rss', 'https://www.newscientist.com/subject/physics/feed/', 'https://www.newscientist.com/subject/technology/feed/', 'https://www.newscientist.com/subject/space/feed/'],
     'Gadgets': ['https://www.xda-developers.com/feed/::all([x not in [y["term"] for y in entry["tags"]] for x in ["Apple", "Oppo", "Vivo", "Poco"]]) and not ("Samsung" in entry["title"] and "Galaxy" in entry["title"]) and not ("Xiaomi" in entry["title"] and ("smartphone" in entry["title"] or "Smartphone" in entry["title"]))', 'https://www.wired.com/feed/category/gear/latest/rss', 'https://www.notebookcheck.net/RSS-Feed-Notebook-Reviews.8156.0.html::all([x not in entry["title"] for x in ["iPhone", "Oppo", "Vivo", "Poco"]]) and not ("Samsung" in entry["title"] and "Galaxy" in entry["title"]) and not ("Xiaomi" in entry["title"] and ("smartphone" in entry["title"] or "Smartphone" in entry["title"]))', 'https://www.notebookcheck.net/News.152.100.html::all([x not in entry["title"] for x in ["iPhone", "Oppo", "Vivo", "Poco"]]) and not ("Samsung" in entry["title"] and "Galaxy" in entry["title"]) and not ("Xiaomi" in entry["title"] and ("smartphone" in entry["title"] or "Smartphone" in entry["title"]))',],    
@@ -11,6 +11,6 @@ const config = {
     'Business': ['https://www.wired.com/feed/category/business/latest/rss', 'https://www.economist.com/finance-and-economics/rss.xml', ],
     'Foss/Self-hosting': ['https://noted.lol/rss/', 'https://console.substack.com/feed', 'https://rss.beehiiv.com/feeds/iiTciQgHPG.xml'],
     'History': ['https://www.heritagedaily.com/feed', 'https://phys.org/rss-feed/science-news/archaeology-fossils'],
-    'News': ['https://indianexpress.com/print/front-page/feed/', 'https://www.mid-day.com/Resources/midday/rss/india-news.xml', 'https://news.abplive.com/news/world/feed',]
+    'News': ['https://indianexpress.com/print/front-page/feed/', 'https://www.mid-day.com/Resources/midday/rss/india-news.xml', 'https://news.abplive.com/news/world/feed', 'swarajyamag.com']
     },
 };
