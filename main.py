1c7e3d24e87ab2873732b1999d9dad6def06eb24
regstuff <6016831+regstuff@users.noreply.github.com>
2022-07-04 09:44:08 +0530

    Add check for rss_feed status == 200

--- main.py
+++ main.py
@@ -21,26 +21,29 @@ for rss_category in config['rssurl']:
         if 'http:' in rss_url or 'https:' in rss_url: rss_feed = feedparser.parse(rss_url) # Proper rss feed. Use feedparser to get the data
         else: rss_feed = feedparser.parse(f"https://news.google.com/rss/search?q=allinurl:{rss_url}+when:{hrsTime}h&ceid=IN:en&hl=en-IN&gl=IN") # Use Google News url (https://newscatcherapi.com/blog/google-news-rss-search-parameters-the-missing-documentaiton)
         
-        rss2json[rss_category_renamed][rss_url] = dict()
-        rss2json[rss_category_renamed][rss_url]['feed'] = dict()
-        rss2json[rss_category_renamed][rss_url]['entries'] = []
-        if 'http:' in rss_url or 'https:' in rss_url: rss2json[rss_category_renamed][rss_url]['feed']['title'] = rss_feed['feed']['title']
-        else: rss2json[rss_category_renamed][rss_url]['feed']['title'] = rss_feed['feed']['title'].split(' ')[0].replace('"allinurl:','') # Get title from rss feed, strtpping out Google search params
-        print(rss2json[rss_category_renamed][rss_url]['feed']['title'])                
-    
-        for entry in rss_feed['entries']:
-            if time_now - time.mktime(entry['published_parsed']) < maxPublishTime*60: # Only add entries within maxPublishedTime
-                entry_dict = {'title': entry['title'], 'summary': re.sub(r'<.*?>', '', entry['summary']), 'link': entry['link'], 'published_js': time.strftime('%Y-%m-%d', entry['published_parsed'])}
-                if 'author' in entry: entry_dict['author'] = entry['author']
-                else: entry_dict['author'] = None
-                if entry['title'] not in all_titles:
-                    all_titles.append(entry['title'])
-                    if '::' in rss_url_full: # Apply filters
-                        if eval(rss_url_full.split('::')[1]): rss2json[rss_category_renamed][rss_url]['entries'].append(entry_dict)
-                    else: rss2json[rss_category_renamed][rss_url]['entries'].append(entry_dict)
+        if rss_feed.status == 200: 
+            rss2json[rss_category_renamed][rss_url] = dict()
+            rss2json[rss_category_renamed][rss_url]['feed'] = dict()
+            rss2json[rss_category_renamed][rss_url]['entries'] = []
+            if 'http:' in rss_url or 'https:' in rss_url: rss2json[rss_category_renamed][rss_url]['feed']['title'] = rss_feed['feed']['title']
+            else: rss2json[rss_category_renamed][rss_url]['feed']['title'] = rss_feed['feed']['title'].split(' ')[0].replace('"allinurl:','') # Get title from rss feed, strtpping out Google search params
+            print(rss2json[rss_category_renamed][rss_url]['feed']['title'])                
 
-                
-        print(len(rss2json[rss_category_renamed][rss_url]['entries']))
+            for entry in rss_feed['entries']:
+                if time_now - time.mktime(entry['published_parsed']) < maxPublishTime*60: # Only add entries within maxPublishedTime
+                    entry_dict = {'title': entry['title'], 'summary': re.sub(r'<.*?>', '', entry['summary']), 'link': entry['link'], 'published_js': time.strftime('%Y-%m-%d', entry['published_parsed'])}
+                    if 'author' in entry: entry_dict['author'] = entry['author']
+                    else: entry_dict['author'] = None
+                    if entry['title'] not in all_titles:
+                        all_titles.append(entry['title'])
+                        if '::' in rss_url_full: # Apply filters
+                            if eval(rss_url_full.split('::')[1]): rss2json[rss_category_renamed][rss_url]['entries'].append(entry_dict)
+                        else: rss2json[rss_category_renamed][rss_url]['entries'].append(entry_dict)
+
+
+            print(len(rss2json[rss_category_renamed][rss_url]['entries']))
+        
+        else: print(rss_url, 'not available')
     
 with open('rss2json.js', 'w') as f: # Dump json into file
     print('Writing JSON to file')
