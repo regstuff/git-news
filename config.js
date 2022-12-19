8d4fc84d3bb2da637c0e980c5ec32c1dcb857ada
regstuff <6016831+regstuff@users.noreply.github.com>
2022-12-19 17:16:54 +0530

    -Wired

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
     'Food/Health': ['https://www.newscientist.com/subject/health/feed/', 'https://phys.org/rss-feed/biology-news/agriculture/', 'http://feeds.feedburner.com/DiscoverMindBrain'],
     'Longform': ['https://www.newscientist.com/section/features/feed/', 'https://feeds.a.dj.com/rss/RSSWSJD.xml', 'https://www.wired.com/feed/category/ideas/latest/rss', 'https://www.wired.com/feed/tag/wired-guide/latest/rss', 'https://www.wired.com/feed/category/backchannel/latest/rss', 'https://www.economist.com/graphic-detail/rss.xml', 'https://www.economist.com/the-economist-explains/rss.xml'],
     'Nature': ['https://usefulscience.org/rss.xml', 'https://www.newscientist.com/subject/earth/feed/', 'https://www.newscientist.com/subject/life/feed/', 'https://www.newscientist.com/subject/humans/feed/', 'https://www.economist.com/science-and-technology/rss.xml', 'http://feeds.feedburner.com/DiscoverLivingWorld', 'http://feeds.feedburner.com/DiscoverEnvironment'],
-    'Business': []//['https://www.wired.com/feed/category/business/latest/rss', 'https://www.economist.com/finance-and-economics/rss.xml', ],
+    'Business': [],//['https://www.wired.com/feed/category/business/latest/rss', 'https://www.economist.com/finance-and-economics/rss.xml', ],
     'Foss/Self-hosting': ['https://console.substack.com/feed', 'https://rss.beehiiv.com/feeds/iiTciQgHPG.xml'],//, 'noted.lol/rss'
     'History': ['https://www.heritagedaily.com/feed', 'https://phys.org/rss-feed/science-news/archaeology-fossils'],
     'News': ['https://indianexpress.com/print/front-page/feed/', 'https://www.mid-day.com/Resources/midday/rss/india-news.xml', 'https://feeds.a.dj.com/rss/RSSWorldNews.xml', 'swarajyamag.com::all([x not in entry["link"] for x in ["/movies/", "/newsletters/"]])']
