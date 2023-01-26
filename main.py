f28c15f3a84679a84bdacab3ee3ec0da125ff91a
regstuff <6016831+regstuff@users.noreply.github.com>
2023-01-26 12:56:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import feedparser, re, time, json
 
 with open('config.js', 'r') as f: # Get config from file
     config = f.read().replace('const config = ','').strip() # Remove Javascript stuff
+    print('1')
     
 config = eval(re.sub(r'((?<!:)//).*?\n','', config).replace('\n','').replace(';','')) # Remove Javascript stuff
+print('config', config)
 
 rss2json = dict()
 all_titles = []
@@ -13,6 +15,7 @@ hrsTime = int(maxPublishTime/60) # For Google News RSS - needed in hours
 time_now = time.time()
 
 for rss_category in config['rssurl']:
+    print(rss_category)
     rss_category_renamed = rss_category.replace('/','_') # If category name has /, class and id names in html will break
     rss2json[rss_category_renamed] = dict()
     for rss_url_full in config['rssurl'][rss_category]:
