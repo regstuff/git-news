74a16024aa52298bfcb08a02d51f8d74a96f438d
regstuff <6016831+regstuff@users.noreply.github.com>
2023-01-26 13:51:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import feedparser, re, time, json
-
+print('0')
 with open('config.js', 'r') as f: # Get config from file
     config = f.read().replace('const config = ','').strip() # Remove Javascript stuff
     print('1')
@@ -20,13 +20,18 @@ for rss_category in config['rssurl']:
     rss2json[rss_category_renamed] = dict()
     for rss_url_full in config['rssurl'][rss_category]:
         rss_url = rss_url_full.split('::')[0]
-
-        try:
-            if 'http:' in rss_url or 'https:' in rss_url: rss_feed = feedparser.parse(rss_url) # Proper rss feed. Use feedparser to get the data
-            else: rss_feed = feedparser.parse(f"https://news.google.com/rss/search?q=allinurl:{rss_url}+when:{hrsTime}h&ceid=IN:en&hl=en-IN&gl=IN") # Use Google News url (https://newscatcherapi.com/blog/google-news-rss-search-parameters-the-missing-documentaiton)
+        print(rss_url)
+        if 'http:' in rss_url or 'https:' in rss_url: feedurl = rss_url # Proper rss feed. Use feedparser to get the data
+        else: feedurl = f"https://news.google.com/rss/search?q=allinurl:{rss_url}+when:{hrsTime}h&ceid=IN:en&hl=en-IN&gl=IN" # Use Google News url (https://newscatcherapi.com/blog/google-news-rss-search-parameters-the-missing-documentaiton)
+        print(feedurl)   
+        try: 
+            print('trying')
+            rss_feed = feedparser.parse(feedurl) # Proper rss feed. Use feedparser to get the data
+            print('done')
         except: print('Unable to get rss feed from', rss_url)
         
         if rss_feed.status == 200: 
+            print('status is 200')
             rss2json[rss_category_renamed][rss_url] = dict()
             rss2json[rss_category_renamed][rss_url]['feed'] = dict()
             rss2json[rss_category_renamed][rss_url]['entries'] = []
