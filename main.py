daa464143bdd1539179c7feaef040c2adb879a08
regstuff <6016831+regstuff@users.noreply.github.com>
2023-10-13 18:08:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import feedparser, re, time, json
-print('0')
+import torch
+from omegaconf import OmegaConf
+from IPython.display import Audio, display
+
+print('Loading Silero models')
+
+torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml', 'latest_silero_models.yml', progress=True)
+models = OmegaConf.load('latest_silero_models.yml')
+
+language = 'en'
+model_id = 'v3_en'
+device = torch.device('cpu')
+model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_tts', language=language, speaker=model_id)
+model.to(device)  # gpu or cpu
+
+print('Loaded Silero models. Configuring voice')
+
+sample_rate = 24000
+speaker = "en_24"
+put_accent=False
+put_yo=False
+example_text = 'Tom Brady. New blood test more convenient for measuring important omega-3 levels'
+
+audio = model.apply_tts(text=example_text, speaker=speaker, sample_rate=sample_rate, put_accent=put_accent, put_yo=put_yo)
+audio_obj = Audio(audio, rate=sample_rate)
+with open('output.mp3', 'wb') as f: f.write(audio_obj.data)
+
+print('Example output.mp3 done.')
+
+print('Loading config.js')
+
 with open('config.js', 'r') as f: # Get config from file
     config = f.read().replace('const config = ','').strip() # Remove Javascript stuff
-    print('1')
+    print('Loaded config.js')
     
 config = eval(re.sub(r'((?<!:)//).*?\n','', config).replace('\n','').replace(';','')) # Remove Javascript stuff
 print('config', config)
@@ -13,9 +43,11 @@ maxPublishTime = eval(str(config['maxPublishTime'])) # in minutes
 hrsTime = int(maxPublishTime/60) # For Google News RSS - needed in hours
 
 time_now = time.time()
+tts_text = ''
 
 for rss_category in config['rssurl']:
     print(rss_category)
+    tts_text += f'New catgeory started. {rss_category}'
     rss_category_renamed = rss_category.replace('/','_') # If category name has /, class and id names in html will break
     rss2json[rss_category_renamed] = dict()
     for rss_url_full in config['rssurl'][rss_category]:
@@ -43,14 +75,20 @@ for rss_category in config['rssurl']:
                 if entry['published_parsed']: 
                     if time_now - time.mktime(entry['published_parsed']) < maxPublishTime*60: # Only add entries within maxPublishedTime
                         entry_dict = dict()
-                        if 'title' in entry: entry_dict['title'] = entry['title']
+                        if 'title' in entry: 
+                            entry_dict['title'] = entry['title']
+                            tts_text += f'New article. {entry_dict["title"]}'
                         else: entry_dict['title'] = 'None'
-                        if 'summary' in entry: entry_dict['summary'] = re.sub(r'<.*?>', '', entry['summary'])
+                        if 'summary' in entry: 
+                            entry_dict['summary'] = re.sub(r'<.*?>', '', entry['summary'])
+                            tts_text += f'{entry_dict["summary"]}'
                         else: entry_dict['summary'] = 'None'
                         if 'link' in entry: entry_dict['link'] = entry['link']
                         else: entry_dict['link'] = 'None'
                         entry_dict['published_js'] = time.strftime('%Y-%m-%d', entry['published_parsed'])
-                        if 'author' in entry: entry_dict['author'] = entry['author']
+                        if 'author' in entry: 
+                            entry_dict['author'] = entry['author']
+                            tts_text += f'Article by {entry_dict["author"]}'
                         else: entry_dict['author'] = 'None'
                         if entry['title'] not in all_titles:
                             all_titles.append(entry['title'])
@@ -66,3 +104,11 @@ for rss_category in config['rssurl']:
 with open('rss2json.js', 'w') as f: # Dump json into file
     print('Writing JSON to file')
     f.write(f'const rss2json = {json.dumps(rss2json)};') # Write to a file that Javascript can use
+
+print('Doing tts on RSS Feed text')
+audio = model.apply_tts(text=tts_text, speaker=speaker, sample_rate=sample_rate, put_accent=put_accent, put_yo=put_yo)
+print('TTS complete. Saving file')
+audio_obj = Audio(audio, rate=sample_rate)
+with open('output.mp3', 'wb') as f: f.write(audio_obj.data)
+print('File saved')
+
