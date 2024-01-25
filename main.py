cf6600def0623c4802826873aff6d4b8dee4dbc3
regstuff <6016831+regstuff@users.noreply.github.com>
2024-01-25 15:00:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,16 @@ for rss_category in config['rssurl']:
             print('REDDIT ENTRIES', entries)
             rss2json[rss_category_renamed][rss_url] = dict()
             rss2json[rss_category_renamed][rss_url]['feed'] = dict()
-            if '.json' not in rss_url: rss2json[rss_category_renamed][rss_url]['entries'] = [{'title': x['data']['title'], 'summary': x['data']['selftext'], 'link': x['data']['url_overridden_by_dest'], 'author': 'None'} for x in entries if time_now-x['data']['created']<maxPublishTime*60]
-            else: rss2json[rss_category_renamed][rss_url]['entries'] = [{'title': x['data']['title'], 'summary': x['data']['selftext'], 'link': x['data']['url'], 'author': 'None'} for x in entries if time_now-x['data']['created']<maxPublishTime*60]
+            
+            clean_title = x['data']['title'].replace('`', '')
+            clean_title = re.sub(r'https?://\S+', '', clean_title)
+            clean_summary = x['data']['selftext'].replace('`', '')
+            clean_summary = re.sub(r'https?://\S+', '', clean_summary)
+            
+            if '.json' not in rss_url: 
+                rss2json[rss_category_renamed][rss_url]['entries'] = [{'title': clean_title, 'summary': clean_summary, 'link': x['data']['url_overridden_by_dest'], 'author': 'None'} for x in entries if time_now-x['data']['created']<maxPublishTime*60]
+            else: 
+                rss2json[rss_category_renamed][rss_url]['entries'] = [{'title': clean_title, 'summary': clean_summary, 'link': x['data']['url'], 'author': 'None'} for x in entries if time_now-x['data']['created']<maxPublishTime*60]
             rss2json[rss_category_renamed][rss_url]['feed']['title'] = 'Reddit - TIL'
 
         elif 'http:' in rss_url or 'https:' in rss_url: feedurl = rss_url # Proper rss feed. Use feedparser to get the data
